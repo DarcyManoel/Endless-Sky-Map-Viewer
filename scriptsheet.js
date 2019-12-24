931f8d7b9a2c0fc70238faa7bbbe2d82c78a0aaa
Darcy Manoel <darcman00@gmail.com>
2019-12-24 13:41:09 +1030

    Condense code while maintaining readability
    And save certain changes in service of system colouring.

--- scriptsheet.js
+++ scriptsheet.js
@@ -1,22 +1,22 @@
 var canvas=document.getElementById("canvas");
 var context=canvas.getContext("2d");
 var government={
-	coalition:"rgb(255,153,179)",
-	efret:"rgb(125,84,176)",
-	hai:"rgb(214,107,207)",
-	heliarch:"rgb(255,204,128)",
-	korath:"rgb(204,128,26)",
-	mereti:"rgb(82,92,166)",
-	pirate:"rgb(199,0,0)",
-	pug:"rgb(252,227,179)",
-	quarg:"rgb(224,196,0)",
-	remnant:"rgb(227,97,158)",
-	republic:"rgb(232,107,23)",
-	sestor:"rgb(191,69,94)",
-	syndicate:"rgb(0,105,181)",
-	unfettered:"rgb(176,84,209)",
-	uninhabited:"rgb(102,102,102)",
-	wanderer:"rgb(179,232,31)",
+	"Hai (Unfettered)":"rgb(176,84,209)",
+	"Kor Efret":"rgb(125,84,176)",
+	"Kor Mereti":"rgb(82,92,166)",
+	"Kor Sestor":"rgb(191,69,94)",
+	Coalition:"rgb(255,153,179)",
+	Hai:"rgb(214,107,207)",
+	Heliarch:"rgb(255,204,128)",
+	Korath:"rgb(204,128,26)",
+	Pirate:"rgb(199,0,0)",
+	Pug:"rgb(252,227,179)",
+	Quarg:"rgb(224,196,0)",
+	Remnant:"rgb(227,97,158)",
+	Republic:"rgb(232,107,23)",
+	Syndicate:"rgb(0,105,181)",
+	Uninhabited:"rgb(102,102,102)",
+	Wanderer:"rgb(179,232,31)",
 };
 var zoom=2.5;
 canvas.height=screen.height;
@@ -25,7 +25,10 @@ function drawSystem(system,faction,xPos,yPos){
 	context.beginPath();
 	context.arc(1750+ +xPos,1250+ +yPos,9,0,2*Math.PI);
 	context.lineWidth=3;
-	context.strokeStyle="#ffffff";
+	if(faction.toString()=="Coalition")
+		{context.strokeStyle="rgb(255,153,179)";}
+	else
+		{context.strokeStyle="#fff";}
 	context.stroke();
 	console.log(system,faction,xPos,yPos);
 }
@@ -42,20 +45,17 @@ function drawSystems(that){
 		var positions4=positions3.split("<br>");
 		positions4.unshift("");
 		var positionsDifference=((positions4.length/2)-systems2.length);
-		if(systems2.length<positions4.length){
-			positions4.splice(positions4.length-positionsDifference,positionsDifference);
-		}
+		if(systems2.length<positions4.length)
+			{positions4.splice(positions4.length-positionsDifference,positionsDifference);}
 		var governments1=lines.split("sys").filter(/./.test,/^tem/).join("<br>").replace(/tem /g,"");
 		var governments2=governments1.split("<br>").filter(/./.test,/^	government/).join("<br>").replace(/government /g,"");
 		var governments3=governments2.split("<br>");
 		var governmentsDifference=(governments3.length-systems2.length);
-		if(systems2.length<governments3.length){
-			governments3.splice(governments3.length-governmentsDifference,governmentsDifference);
-		}
+		if(systems2.length<governments3.length)
+			{governments3.splice(governments3.length-governmentsDifference,governmentsDifference);}
 		var i;
-		for (i=0;i<systems2.length;i++){
-			drawSystem(systems2[i],governments3[i],positions4[((i+1)*2)-1],positions4[(i+1)*2]);
-		}
+		for(i=0;i<systems2.length;i++)
+			{drawSystem(systems2[i],governments3[i],positions4[((i+1)*2)-1],positions4[(i+1)*2]);}
 		document.getElementById("output").innerHTML=systems2.join("<br>");
 		console.log("Systems: "+systems2.length);
 		console.log("Positions: "+((positions4.length-1)/2));
