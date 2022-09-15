5d0f5a5618f013488944455d5401396d795928aa
Darcy Manoel <darcman00@gmail.com>
2022-09-15 17:09:29 +0930

    FIX: The selected systems details will persist indefinitely

--- backend/main.js
+++ backend/main.js
@@ -109,22 +109,22 @@ function drawHUD(){
 			}
 		}
 		HUDContext.drawImage(trade,0,planet.height*1.299*scale*accessiblePlanets+system.height*1.4*scale,trade.width*1.4*scale,trade.height*1.4*scale)
-		document.getElementById(`tradeContainer`).innerHTML=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:10px;line-height:140%;position:absolute;top:`+parseInt(99+(120*accessiblePlanets))+`px;">`+elements[0][target][5][0].join(`<br>`)+`</label>`
+		document.getElementById(`tradeContainer`).innerHTML=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:10px;line-height:140%;position:absolute;top:`+parseInt(99+(121*accessiblePlanets))+`px;">`+elements[0][target][5][0].join(`<br>`)+`</label>`
 		if(systemSelected){
 			for(i2=0;i2<elements[0][target][5][0].length;i2++){
 				if(elements[0][target][5][1][i2]>elements[0][systemSelected][5][1][i2]){
-					document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(88,166,88);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+parseInt(99+(120*accessiblePlanets)+(18*i2))+`px;width:30px;">+`+eval(elements[0][target][5][1][i2]-elements[0][systemSelected][5][1][i2])+`</label>`
+					document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(88,166,88);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+parseInt(99+(121*accessiblePlanets)+(18*i2))+`px;width:30px;">+`+eval(elements[0][target][5][1][i2]-elements[0][systemSelected][5][1][i2])+`</label>`
 				}
 				else if(elements[0][target][5][1][i2]<elements[0][systemSelected][5][1][i2]){
-					document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(166,88,88);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+parseInt(99+(120*accessiblePlanets)+(18*i2))+`px;width:30px;">`+eval(elements[0][target][5][1][i2]-elements[0][systemSelected][5][1][i2])+`</label>`
+					document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(166,88,88);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+parseInt(99+(121*accessiblePlanets)+(18*i2))+`px;width:30px;">`+eval(elements[0][target][5][1][i2]-elements[0][systemSelected][5][1][i2])+`</label>`
 				}
 				else if(elements[0][target][5][1][i2]==elements[0][systemSelected][5][1][i2]){
-					document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+parseInt(99+(120*accessiblePlanets)+(18*i2))+`px;width:30px;">`+elements[0][target][5][1][i2]+`</label>`
+					document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+parseInt(99+(121*accessiblePlanets)+(18*i2))+`px;width:30px;">`+elements[0][target][5][1][i2]+`</label>`
 				}
 			}
 		}
 		else{
-			document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+parseInt(99+(120*accessiblePlanets))+`px;width:30px;">`+elements[0][target][5][1].join(`<br>`)+`</label>`
+			document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+parseInt(99+(121*accessiblePlanets))+`px;width:30px;">`+elements[0][target][5][1].join(`<br>`)+`</label>`
 		}
 		HUDContext.drawImage(zoom,canvas.width*scale*2.8,canvas.height*scale*1.6,zoom.width*1.4*scale,zoom.height*1.4*scale)
 		drawSelect(elements[0][target][1][0]-galaxyPosition[0],elements[0][target][1][1]-galaxyPosition[1])
@@ -141,11 +141,39 @@ function drawHUD(){
 		document.getElementById(`governmentDisplay`).innerHTML=`- government -`
 		document.getElementById(`governmentDisplay`).style.color=`rgb(102,102,102)`
 		document.getElementById(`planetsContainer`).innerHTML=``
-		HUDContext.drawImage(trade,0,planet.height*1.299*scale*accessiblePlanets+system.height*1.4*scale,trade.width*1.4*scale,trade.height*1.4*scale)
-		document.getElementById(`tradeContainer`).innerHTML=``
-		document.getElementById(`tradeContainer`).innerHTML+=`<label onClick="resetSelected()" style="animation:none;color:rgb(102,102,102);font-size:13px;left:10px;line-height:140%;position:absolute;top:`+99+`px;width:100px;">`+elements[0][target][5][0].join(`<br>`)+`</label>`
+		accessiblePlanets=0
 		if(systemSelected){
-			document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+99+`px;width:30px;">`+elements[0][systemSelected][5][1].join(`<br>`)+`</label>`
+			if(elements[0][systemSelected][4].length){
+				for(i2=0;i2<elements[0][systemSelected][4].length;i2++){
+					for(i3=0;i3<elements[3].length;i3++){
+						if(elements[0][systemSelected][4][i2]==elements[3][i3][0]){
+							if(!elements[3][i3][1].includes(`requires: inaccessible`)){
+								HUDContext.drawImage(planet,0,planet.height*1.299*scale*accessiblePlanets+system.height*1.4*scale,planet.width*1.4*scale,planet.height*1.4*scale)
+								document.getElementById(`planetsContainer`).innerHTML+=`<label style="animation:none;color:rgb(112,112,112);font-size:13px;height:15px;left:29px;overflow:hidden;position:absolute;top:`+parseInt(planet.height*1.299*scale*accessiblePlanets/3+system.height*1.4*scale*.4)+`px;width:150px;">`+elements[0][systemSelected][4][i2]+`</label>`
+								if(elements[3][i3][3]){
+									document.getElementById(`planetsContainer`).innerHTML+=`<label style="animation:none;font-size:13px;left:38px;position:absolute;top:`+parseInt(planet.height*1.299*scale*accessiblePlanets/3+system.height*1.4*scale*.475)+`px;width:150px;">Shipyard</label>`
+								}
+								else{
+									document.getElementById(`planetsContainer`).innerHTML+=`<label style="animation:none;color:rgb(70,70,70);font-size:13px;left:38px;position:absolute;top:`+parseInt(planet.height*1.299*scale*accessiblePlanets/3+system.height*1.4*scale*.475)+`px;width:150px;">Shipyard</label>`
+								}
+								if(elements[3][i3][4]){
+									document.getElementById(`planetsContainer`).innerHTML+=`<label style="animation:none;font-size:13px;left:38px;position:absolute;top:`+parseInt(planet.height*1.299*scale*accessiblePlanets/3+system.height*1.4*scale*.55)+`px;width:150px;">Outfitter</label>`
+								}
+								else{
+									document.getElementById(`planetsContainer`).innerHTML+=`<label style="animation:none;color:rgb(70,70,70);font-size:13px;left:38px;position:absolute;top:`+parseInt(planet.height*1.299*scale*accessiblePlanets/3+system.height*1.4*scale*.55)+`px;width:150px;">Outfitter</label>`
+								}
+								accessiblePlanets++
+							}
+						}
+					}
+				}
+			}
+		}
+		HUDContext.drawImage(trade,0,planet.height*1.299*scale*accessiblePlanets+system.height*1.4*scale,trade.width*1.4*scale,trade.height*1.4*scale)
+		document.getElementById(`tradeContainer`).innerHTML=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:10px;line-height:140%;position:absolute;top:`+99+`px;width:100px;">`+elements[0][target][5][0].join(`<br>`)+`</label>`
+		if(systemSelected){
+			document.getElementById(`tradeContainer`).innerHTML=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:10px;line-height:140%;position:absolute;top:`+parseInt(99+(121*accessiblePlanets))+`px;">`+elements[0][systemSelected][5][0].join(`<br>`)+`</label>`
+			document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+parseInt(99+(121*accessiblePlanets))+`px;width:30px;">`+elements[0][systemSelected][5][1].join(`<br>`)+`</label>`
 		}
 		else{
 			document.getElementById(`tradeContainer`).innerHTML+=`<label style="animation:none;color:rgb(102,102,102);font-size:13px;left:110px;line-height:140%;position:absolute;text-align:right;top:`+99+`px;width:30px;">-<br>-<br>-<br>-<br>-<br>-<br>-<br>-<br>-<br>-</label>`
