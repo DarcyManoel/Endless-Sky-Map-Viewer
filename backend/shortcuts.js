51bc49f7047c9ee0cf5917a8ba5084b7e817a4a7
Darcy Manoel <darcman00@gmail.com>
2022-09-24 18:01:28 +0930

    Reorganise shortcuts functions

--- backend/shortcuts.js
+++ backend/shortcuts.js
@@ -1,14 +1,4 @@
-//	Draw a fake link on the hud for possible new linked systems
-function drawFakeLink(startX,startY,endX,endY){
-	HUDContext.beginPath()
-	HUDContext.moveTo(2150*scale+ +startX-galaxyPosition[0],1350*scale+ +startY-galaxyPosition[1])
-	HUDContext.lineTo(2150*scale+ +endX,1350*scale+ +endY)
-	HUDContext.setLineDash([0,15,0])
-	HUDContext.lineWidth=2
-	HUDContext.strokeStyle=`rgb(102,102,102)`
-	HUDContext.stroke()
-}
-//	Draw a link on the map
+//	Draw objects on map
 function drawLink(startX,startY,endX,endY){
 	canvasContext.beginPath()
 	canvasContext.moveTo(2150*scale+ +startX-galaxyPosition[0],1350*scale+ +startY-galaxyPosition[1])
@@ -23,7 +13,6 @@ function drawLink(startX,startY,endX,endY){
 	canvasContext.strokeStyle=`rgb(102,102,102)`
 	canvasContext.stroke()
 }
-//	Draw a coloured link on the map
 function drawLinkColour(startX,startY,endX,endY,systemGovernment){
 	canvasContext.beginPath()
 	canvasContext.moveTo(2150*scale+ +startX-galaxyPosition[0],1350*scale+ +startY-galaxyPosition[1])
@@ -33,7 +22,41 @@ function drawLinkColour(startX,startY,endX,endY,systemGovernment){
 	canvasContext.strokeStyle=`rgb(`+systemGovernment[0]*255+`,`+systemGovernment[1]*255+`,`+systemGovernment[2]*255+`)`
 	canvasContext.stroke()
 }
-//	Draw jump range on the overlay
+function drawSystem(x,y,radius){
+	canvasContext.beginPath()
+	canvasContext.arc(2150*scale+ +x-galaxyPosition[0],1350*scale+ +y-galaxyPosition[1],radius,0,2*Math.PI)
+	canvasContext.setLineDash([])
+	canvasContext.lineWidth=3.6
+	canvasContext.strokeStyle=`rgb(102,102,102)`
+	canvasContext.stroke()
+}
+function drawSystemColour(x,y,radius,systemGovernment){
+	canvasContext.beginPath()
+	canvasContext.arc(2150*scale+ +x-galaxyPosition[0],1350*scale+ +y-galaxyPosition[1],radius,0,2*Math.PI)
+	canvasContext.setLineDash([])
+	canvasContext.lineWidth=3.6
+	canvasContext.strokeStyle=`rgb(`+systemGovernment[0]*255+`,`+systemGovernment[1]*255+`,`+systemGovernment[2]*255+`)`
+	canvasContext.stroke()
+}
+function drawWormhole(startX,startY,endX,endY){
+	canvasContext.beginPath()
+	canvasContext.moveTo(2150*scale+ +startX-galaxyPosition[0],1350*scale+ +startY-galaxyPosition[1])
+	canvasContext.lineTo(2150*scale+ +endX-galaxyPosition[0],1350*scale+ +endY-galaxyPosition[1])
+	canvasContext.setLineDash([0,15,10000])
+	canvasContext.lineWidth=2
+	canvasContext.strokeStyle=`rgb(128,51,230)`
+	canvasContext.stroke()
+}
+//	Draw HUD elements
+function drawFakeLink(startX,startY,endX,endY){
+	HUDContext.beginPath()
+	HUDContext.moveTo(2150*scale+ +startX-galaxyPosition[0],1350*scale+ +startY-galaxyPosition[1])
+	HUDContext.lineTo(2150*scale+ +endX,1350*scale+ +endY)
+	HUDContext.setLineDash([0,15,0])
+	HUDContext.lineWidth=2
+	HUDContext.strokeStyle=`rgb(102,102,102)`
+	HUDContext.stroke()
+}
 function drawRange(x,y){
 	HUDContext.beginPath()
 	HUDContext.arc(2150*scale+ +x-galaxyPosition[0],1350*scale+ +y-galaxyPosition[1],100,0,2*Math.PI)
@@ -42,7 +65,6 @@ function drawRange(x,y){
 	HUDContext.strokeStyle=`rgb(102,102,102)`
 	HUDContext.stroke()
 }
-//	Draw jump range on the overlay
 function drawRestricted(x,y){
 	if(systemBuffer==true){
 		HUDContext.beginPath()
@@ -63,7 +85,6 @@ function drawRestricted(x,y){
 	HUDContext.strokeStyle=`rgb(102,102,102)`
 	HUDContext.stroke()
 }
-//	Draw encompassing circle
 function drawSelect(x,y){
 	HUDContext.beginPath()
 	switch(mapStyle){
@@ -78,34 +99,6 @@ function drawSelect(x,y){
 	HUDContext.lineWidth=2
 	HUDContext.strokeStyle=`rgb(255,255,255)`
 	HUDContext.stroke()
-}
-//	Draw a system on the map
-function drawSystem(x,y,radius){
-	canvasContext.beginPath()
-	canvasContext.arc(2150*scale+ +x-galaxyPosition[0],1350*scale+ +y-galaxyPosition[1],radius,0,2*Math.PI)
-	canvasContext.setLineDash([])
-	canvasContext.lineWidth=3.6
-	canvasContext.strokeStyle=`rgb(102,102,102)`
-	canvasContext.stroke()
-}
-//	Draw a coloured system on the map
-function drawSystemColour(x,y,radius,systemGovernment){
-	canvasContext.beginPath()
-	canvasContext.arc(2150*scale+ +x-galaxyPosition[0],1350*scale+ +y-galaxyPosition[1],radius,0,2*Math.PI)
-	canvasContext.setLineDash([])
-	canvasContext.lineWidth=3.6
-	canvasContext.strokeStyle=`rgb(`+systemGovernment[0]*255+`,`+systemGovernment[1]*255+`,`+systemGovernment[2]*255+`)`
-	canvasContext.stroke()
-}
-//	Draw a wormhole on the map
-function drawWormhole(startX,startY,endX,endY){
-	canvasContext.beginPath()
-	canvasContext.moveTo(2150*scale+ +startX-galaxyPosition[0],1350*scale+ +startY-galaxyPosition[1])
-	canvasContext.lineTo(2150*scale+ +endX-galaxyPosition[0],1350*scale+ +endY-galaxyPosition[1])
-	canvasContext.setLineDash([0,15,10000])
-	canvasContext.lineWidth=2
-	canvasContext.strokeStyle=`rgb(128,51,230)`
-	canvasContext.stroke()
 }
 //	Calculate distance between two unaligned points
 Math.dist=function(x1,y1,x2,y2){ 
