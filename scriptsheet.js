1876073507b46ef48efc8cfe4e6b986d53ac23f5
DarcyManoel <darcman00@gmail.com>
2023-10-01 12:28:03 +1030

    Condense distance shortcut

--- scriptsheet.js
+++ scriptsheet.js
@@ -792,14 +792,8 @@ function zoomIn(){
 	drawMap()
 }
 //	Shortcuts
-Math.dist=function(x1,y1,x2,y2){ 
-	if(!x2){
-		x2=0
-	}
-	if(!y2){
-		y2=0
-	}
-	return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
+Math.dist=function(x1,y1,x2,y2){
+	return Math.sqrt((+x2-+x1)*(+x2-+x1)+(+y2-+y1)*(+y2-+y1))
 }
 function splitLastOccurrence(string,substring) {
 	var lastIndex=string.lastIndexOf(substring);
